1b8569512b27905336e70e56c25b20edbc95e622
Caue Waneck <waneck@gmail.com>
2013-04-23 18:07:40 -0300

    Update tests to Haxe 3

--- haxelib/test/BasicTests.hx
+++ haxelib/test/BasicTests.hx
@@ -8,56 +8,54 @@ import hxffi.dlib.DynamicLib;
 import haxe.io.Bytes;
 #if cpp
 import cpp.Lib;
-import cpp.Sys;
 #else
 import neko.Lib;
-import neko.Sys;
 #end
 
 
 class BasicTests
 {
-	
+
 	public function new():Void
 	{
-		
+
 	}
-	
+
 	public function setup():Void
 	{
-		
+
 	}
-	
+
 	public function test_InvalidLoad():Void
 	{
 		var lib = DynamicLib.getLib("INVALIDLOCATION");
 		Assert.isNull(lib);
 	}
-	
+
 	public function test_DynamicLoad():Void
 	{
 		var lib = DynamicLib.getLib(Sys.getCwd() + "hxffi.ndll");
 		Assert.notNull(lib);
 		var pointer = lib.getSymbol("checking");
 		Assert.notNull(pointer);
-		
+
 		var cif = CallInterface.ofNativeTypes([TInt, TShort, TChar], TInt);
 		var ret = Bytes.alloc(4); //just to be safe
-		
+
 		cif.call(pointer, ret.getData(), -6,-12,-1);
-		
+
 		var b = new haxe.io.BytesInput(ret);
-		var ret = b.readInt31();
+		var ret = b.readInt32();
 		Assert.notEquals(0, ret);
 		Assert.equals(1, ret); //should be 1; but there can be endianness issues here that we won't take care right now (structs will take care)
 	}
-	
+
 	public function test_CallOnC():Void
 	{
 		var lib = Lib.load("hxffi", "hxffi_test_ffi", 0);
 		Assert.equals(0, lib()); //just doesn't fail it's good
 	}
-	
+
 	public function test_CallInternal():Void
 	{
 		/*
@@ -95,21 +93,21 @@ class BasicTests
 		if (rint != 0)
 			neko_error();
 		*/
-		
+
 		var cif = CallInterface.ofNativeTypes([TInt, TShort, TChar], TInt);
 		var pointer = FunctionPointer.withHandle(checking_addr());
 		var ret = Bytes.alloc(4); //just to be safe
-		
+
 		//testing call dynamic
 		cif.callDynamic(pointer, ret.getData(), [-6,-12,-1]);
-		
+
 		var b = new haxe.io.BytesInput(ret);
-		var ret = b.readInt31();
+		var ret = b.readInt32();
 		Assert.notEquals(0, ret);
 		Assert.equals(1, ret); //should be 1; but there can be endianness issues here that we won't take care right now (structs will take care)
-		
+
 		trace(ret);
 	}
-	
+
 	static var checking_addr = Lib.load("hxffi", "hxffi_get_address_checking", 0);
 }
