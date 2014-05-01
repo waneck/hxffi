f7f7a6ba51d440b380e9d4bd5ce94b532849fa5a
Cauê Waneck <waneck@gmail.com>
2014-04-30 22:43:02 -0300

    sync

--- test/BasicTests.hx
+++ test/BasicTests.hx
@@ -20,42 +20,20 @@ class BasicTests
 
 	public function new():Void
 	{
-		lib = DynamicLib.getLib('bin/testlib.so');
-		Assert.notNull(lib);
+		if (sys.FileSystem.exists('bin/testlib.so'))
+			lib = DynamicLib.getLib('bin/testlib.so');
+		else
+			lib = DynamicLib.getLib(Sys.executablePath() + '/testlib.so');
 	}
 
 	public function setup():Void
 	{
-
+		Assert.notNull(lib);
 	}
 
 	public function test_InvalidLoad():Void
 	{
 		var lib = DynamicLib.getLib("INVALIDLOCATION");
 		Assert.isNull(lib);
-	}
-
-	public function test_DynamicLoad():Void
-	{
-		var lib = DynamicLib.getLib(Sys.getCwd() + "hxffi.ndll");
-		Assert.notNull(lib);
-		var pointer = lib.getSymbol("checking");
-		Assert.notNull(pointer);
-
-		var cif = CallInterface.ofNativeTypes([TInt, TShort, TChar], TInt);
-		var ret = Bytes.alloc(4); //just to be safe
-
-		cif.call(pointer, ret.getData(), -6,-12,-1);
-
-		var b = new haxe.io.BytesInput(ret);
-		var ret = b.readInt32();
-		Assert.notEquals(0, ret);
-		Assert.equals(1, ret); //should be 1; but there can be endianness issues here that we won't take care right now (structs will take care)
-	}
-
-	public function test_CallOnC():Void
-	{
-		var lib = Lib.load("hxffi", "hxffi_test_ffi", 0);
-		Assert.equals(0, lib()); //just doesn't fail it's good
 	}
 }
